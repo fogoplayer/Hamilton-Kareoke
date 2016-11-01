7afd7f6e21aca30092ef6e5500f3d52e53ec45b5
fogoplayer <fogoplayer@gmail.com>
2016-11-01 14:40:10 -0400

    commit

--- main.js
+++ main.js
@@ -176,7 +176,7 @@ var changeSong = function(direction) {
   }else{
     hashValue = hash.substring(hash.length - 1, hash.length);
   }
-  console.log("hash" + hash.substring(hash.length - 1, hash.length));
+  console.log("hash" + window.location.hash.substring(hash.length - 1, hash.length));
   loadLyrics(reference, hashValue);
 };
 
